732f7fb31fe79f0fc67341048ddc9fdbfd7fe3f9
pupil <pupil@ivkhk.ee>
2023-03-30 14:28:07 +0300

    Update

---
+++ Music/app/src/main/java/com/example/music/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.music;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.button);
+        btn2 = findViewById(R.id.button2);
+        btn3 = findViewById(R.id.button3);
+
+        btn1.setOnClickListener(view -> {
+            goToScreen(1);
+        });
+        btn2.setOnClickListener(view -> {
+            goToScreen(2);
+        });
+        btn3.setOnClickListener(view -> {
+            goToScreen(3);
+        });
+    }
+
+    private void goToScreen(int screenNumber) {
+        switch (screenNumber) {
+            case 1:
+                startActivity(new Intent(this, TrashMetal.class));
+                break;
+            case 2:
+                startActivity(new Intent(this, Synthwave.class));
+                break;
+            case 3:
+                startActivity(new Intent(this, ElectroSwing.class));
+                break;
+        }
+    }
+}
--- SecondActivity/app/src/main/java/com/example/secondactivity/SecondActivity.java
+++ SecondActivity/app/src/main/java/com/example/secondactivity/SecondActivity.java
@@ -8,13 +8,23 @@ import android.widget.TextView;
 public class SecondActivity extends AppCompatActivity {
 
     private TextView txt;
+    private TextView txt2;
+    private TextView txt3;
+    private TextView txt4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        txt = findViewById(R.id.txt);
-        txt.setText(getIntent().getStringExtra("message"));
+        txt = findViewById(R.id.textView);
+        txt2 = findViewById(R.id.textView2);
+        txt3 = findViewById(R.id.textView3);
+        txt4 = findViewById(R.id.textView4);
+
+        txt.setText(getIntent().getStringExtra("name"));
+        txt2.setText(getIntent().getStringExtra("surname"));
+        txt3.setText(getIntent().getStringExtra("phone"));
+        txt4.setText(getIntent().getStringExtra("adress"));
     }
 }
